b552a146275078dbcbb294357d9fbf729e5a1359
Endi Oliveira <100172961+endioliveira@users.noreply.github.com>
2023-03-17 17:09:04 -0300

    organizando o codigo

--- src/business/PostBusiness.ts
+++ src/business/PostBusiness.ts
@@ -2,10 +2,11 @@ import { PostDatabase } from "../database/PostDatabase";
 import { Post } from "../models/Post"
 import { Comment } from "../models/Comment";
 import { BadRequestError } from '../errors/BadRequestError'
-import { CreateCommentDTO, CreatePostInputDTO, GetPostById, GetPostsInputDTO, GetPostsOutputDTO, GetPostWithComments } from "../dtos/PostDTO";
+import { CreateCommentDTO, CreatePostInputDTO, GetCommentById, GetPostById, GetPostsInputDTO, GetPostsOutputDTO, GetPostWithComments } from "../dtos/PostDTO";
 import { IdGenerator } from "../services/IdGenerator";
 import { TokenManager } from "../services/TokenManager";
-import { PostCreatorDB, PostWithComments, POST_LIKE, COMMENT_LIKE, LikeDislikePostDB, LikesDislikeCommentsDB } from "../types";
+import { CommentDB, PostCommentModel, PostCreatorDB, PostModel, PostWithComments, POST_LIKE, COMMENT_LIKE, LikeDislikePostDB, LikesDislikeCommentsDB } from "../types";
+import { CommentByPostId } from "../models/CommentByPostId";
 import { LikePostInputDTO } from "../dtos/PostLikesDTO"
 import { LikePost } from "../models/LikePost"
 import { LikeCommentInputDTO } from "../dtos/CommentLikesDTO";
@@ -115,6 +116,32 @@ export class PostBusiness {
         return output
 
     }
+
+    // public getPostComments = async (input: GetPostById): Promise<PostModel> => {
+
+    //     const { postId, token } = input
+
+    //     if (token === undefined) {
+    //         throw new BadRequestError("Insira o 'token'")
+    //     }
+
+    //     const payload = this.tokenManager.getPayload(token)
+
+    //     if (payload === null) {
+    //         throw new BadRequestError("O 'token' não é válido!")
+    //     }
+
+    //     const postExist = await this.postDatabase.findPostById(postId)
+
+    //     if (!postExist) {
+    //         throw new BadRequestError("O 'id' não foi encontrado!")
+    //     }
+
+
+    //     return postExist
+
+    // }
+
 
     public createComment = async (input: CreateCommentDTO): Promise<void> => {
 
